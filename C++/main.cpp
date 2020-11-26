b0872b688b2df7d1e4e4592b632407782dabe95b
songhee <20150261sh@gmail.com>
2020-11-26 23:00:26 +0900

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by songhee on 2020/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
